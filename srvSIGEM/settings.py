191c0313ff65b5121065ca205ebea1dc2c81b0dc
tallarin <benja.gonzalezs@duocuc.cl>
2025-05-07 23:35:28 -0400

    appSigem

--- srvSIGEM/settings.py
+++ srvSIGEM/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'srvSIGEM.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'paginas' / 'templates'],  # Ruta al directorio templates de la app
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
