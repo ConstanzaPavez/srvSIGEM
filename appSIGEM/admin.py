d6d5a42dcb51f03ed1965edd7f34e1da91c66b69
tallarin <benja.gonzalezs@duocuc.cl>
2025-05-12 17:20:03 -0400

    BDADMIN

--- appSIGEM/admin.py
+++ appSIGEM/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CategoriaDj, TipoMaterial, Marca, Material
 
-# Register your models here.
+# Registrar los modelos para que aparezcan en el admin de Django
+admin.site.register(CategoriaDj)
+admin.site.register(TipoMaterial)
+admin.site.register(Marca)
+admin.site.register(Material)
